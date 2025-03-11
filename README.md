3b242df3691ad26b7ca3006fa204680e973aa579
Atlanmail <tiep123@trieuvan.com>
2025-03-11 06:40:31 -0700

    updated gitignore

---
+++ README.md
@@ -0,0 +1,6 @@
+---
+title: Yolo_Sandbox
+app_file: run.py
+sdk: gradio
+sdk_version: 5.20.1
+---
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from src.yolo import detectObjects
 demo = gradio.Interface(fn=detectObjects, inputs=gradio.Image(type="pil"), outputs="image")
     
 if __name__ == "__main__":
-    demo.launch(share=False)
+    demo.launch(share=True)
